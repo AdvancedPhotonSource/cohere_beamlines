6d77e6ab0dff54a414541281f20dc74ddcad4046
bfrosik <bfrosik@anl.gov>
2026-01-20 19:21:00 -0600

    added beamline aps_20ide, still in test

---
+++ src/cohere_beamlines/aps_20ide/beam_tabs.py
@@ -0,0 +1,377 @@
+# #########################################################################
+# Copyright (c) , UChicago Argonne, LLC. All rights reserved.             #
+#                                                                         #
+# See LICENSE file.                                                       #
+# #########################################################################
+
+import os
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import ast
+import cohere_core.utilities as ut
+
+
+def msg_window(text):
+    """
+    Shows message with requested information (text)).
+    Parameters
+    ----------
+    text : str
+        string that will show on the screen
+    Returns
+    -------
+    noting
+    """
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Information)
+    msg.setText(text)
+    msg.setWindowTitle("Info")
+    msg.exec_()
+
+
+def select_file(start_dir):
+    """
+    Shows dialog interface allowing user to select file from file system.
+    Parameters
+    ----------
+    start_dir : str
+        directory where to start selecting the file
+    Returns
+    -------
+    str
+        name of selected file or None
+    """
+    start_dir = start_dir.replace(os.sep, '/')
+    dialog = QFileDialog(None, 'select dir', start_dir)
+    dialog.setFileMode(QFileDialog.ExistingFile)
+    dialog.setSidebarUrls([QUrl.fromLocalFile(start_dir)])
+    if dialog.exec_() == QDialog.Accepted:
+        return str(dialog.selectedFiles()[0]).replace(os.sep, '/')
+    else:
+        return None
+
+
+def select_dir(start_dir):
+    """
+    Shows dialog interface allowing user to select directory from file system.
+    Parameters
+    ----------
+    start_dir : str
+        directory where to start selecting
+    Returns
+    -------
+    str
+        name of selected directory or None
+    """
+    start_dir = start_dir.replace(os.sep, '/')
+    dialog = QFileDialog(None, 'select dir', start_dir)
+    dialog.setFileMode(QFileDialog.DirectoryOnly)
+    dialog.setSidebarUrls([QUrl.fromLocalFile(start_dir)])
+    if dialog.exec_() == QDialog.Accepted:
+        return str(dialog.selectedFiles()[0]).replace(os.sep, '/')
+    else:
+        return None
+
+
+def set_overriden(item):
+    """
+    Helper function that will set the text color to black.
+    Parameters
+    ----------
+    item : widget
+    Returns
+    -------
+    nothing
+    """
+    item.setStyleSheet('color: black')
+
+class InstrTab(QWidget):
+    def __init__(self, parent=None):
+        """
+        Constructor, initializes the tabs.
+        """
+        super(InstrTab, self).__init__(parent)
+        self.name = 'Instrument'
+        self.conf_name = 'config_instr'
+
+    def init(self, tabs, main_window):
+        """
+        Creates and initializes the 'Instrument' tab.
+        Parameters
+        ----------
+        none
+        Returns
+        -------
+        nothing
+        """
+        self.tabs = tabs
+        self.main_win = main_window
+
+        tab_layout = QVBoxLayout()
+        gen_layout = QFormLayout()
+        self.diffractometer = QLineEdit()
+        gen_layout.addRow("diffractometer", self.diffractometer)
+        self.data_dir_button = QPushButton()
+        gen_layout.addRow("data directory", self.data_dir_button)
+        self.dark_file_button = QPushButton()
+        gen_layout.addRow("darkfield file", self.dark_file_button)
+        self.white_file_button = QPushButton()
+        gen_layout.addRow("whitefield file", self.white_file_button)
+        self.Imult = QLineEdit()
+        gen_layout.addRow("Imult", self.Imult)
+        self.detector = QLineEdit()
+        gen_layout.addRow("detector", self.detector)
+        self.roi = QLineEdit()
+        gen_layout.addRow("detector area (roi)", self.roi)
+        self.energy = QLineEdit()
+        gen_layout.addRow("energy", self.energy)
+        self.DetZ = QLineEdit()
+        gen_layout.addRow("DetZ (mm)", self.DetZ)
+        self.VFF_ETA = QLineEdit()
+        gen_layout.addRow("VFF_ETA", self.VFF_ETA)
+        self.VFF_R = QLineEdit()
+        gen_layout.addRow("VFF_R", self.VFF_R)
+        # self.samRy = QLineEdit()
+        # gen_layout.addRow("samRy", self.samRy)
+        tab_layout.addLayout(gen_layout)
+        cmd_layout = QHBoxLayout()
+        self.set_instr_conf_from_button = QPushButton("Load instr conf from")
+        self.set_instr_conf_from_button.setStyleSheet("background-color:rgb(205,178,102)")
+        self.save_instr_conf = QPushButton('save config', self)
+        self.save_instr_conf.setStyleSheet("background-color:rgb(175,208,156)")
+        cmd_layout.addWidget(self.set_instr_conf_from_button)
+        cmd_layout.addWidget(self.save_instr_conf)
+        tab_layout.addLayout(cmd_layout)
+        tab_layout.addStretch()
+        self.setLayout(tab_layout)
+
+        self.data_dir_button.clicked.connect(self.set_data_dir)
+        self.dark_file_button.clicked.connect(self.set_dark_file)
+        self.white_file_button.clicked.connect(self.set_white_file)
+        self.save_instr_conf.clicked.connect(self.save_conf)
+        self.set_instr_conf_from_button.clicked.connect(self.load_instr_conf)
+
+
+    def run_tab(self):
+        pass
+
+
+    def load_tab(self, conf_map):
+        """
+        It verifies given configuration file, reads the parameters, and fills out the window.
+        Parameters
+        ----------
+        conf : dict
+            configuration (config_instr)
+        Returns
+        -------
+        nothing
+        """
+        if 'diffractometer' in conf_map:
+            diff = str(conf_map['diffractometer']).replace(" ", "")
+            self.diffractometer.setText(diff)
+        if 'data_dir' in conf_map:
+            if os.path.isdir(conf_map['data_dir']):
+                self.data_dir_button.setStyleSheet("Text-align:left")
+                self.data_dir_button.setText(conf_map['data_dir'])
+            else:
+                msg_window(f'The data_dir directory in config_prep file {conf_map["data_dir"]} does not exist')
+        else:
+            self.data_dir_button.setText('')
+        if 'darkfield_filename' in conf_map:
+            if os.path.isfile(conf_map['darkfield_filename']):
+                self.dark_file_button.setStyleSheet("Text-align:left")
+                self.dark_file_button.setText(conf_map['darkfield_filename'])
+            else:
+                self.dark_file_button.setText('')
+                msg_window(f'The darkfield file {conf_map["darkfield_filename"]} in config_prep file does not exist')
+        else:
+            self.dark_file_button.setText('')
+        if 'whitefield_filename' in conf_map:
+            if os.path.isfile(conf_map['whitefield_filename']):
+                self.white_file_button.setStyleSheet("Text-align:left")
+                self.white_file_button.setText(conf_map['whitefield_filename'])
+            else:
+                self.white_file_button.setText('')
+                msg_window(f'The whitefield file {conf_map["whitefield_filename"]} in config_prep file does not exist')
+        else:
+            self.white_file_button.setText('')
+        if 'Imult' in conf_map:
+            self.Imult.setText(str(conf_map['Imult']).replace(" ", ""))
+        if 'detector' in conf_map:
+            self.detector.setText(str(conf_map['detector']).replace(" ", ""))
+            self.detector.setStyleSheet('color: black')
+        if 'roi' in conf_map:
+            self.roi.setText(str(conf_map['roi']).replace(" ", ""))
+            self.roi.setStyleSheet('color: black')
+        if 'vff_r_offset' in conf_map:
+            diff = str(conf_map['vff_r_offset']).replace(" ", "")
+            self.VFF_R.setText(diff)
+        if 'vff_eta_offset' in conf_map:
+            diff = str(conf_map['vff_eta_offset']).replace(" ", "")
+            self.VFF_ETA.setText(diff)
+        if 'energy' in conf_map:
+            self.energy.setText(str(conf_map['energy']).replace(" ", ""))
+            self.energy.setStyleSheet('color: black')
+        if 'DetZ' in conf_map:
+            self.DetZ.setText(str(conf_map['DetZ']).replace(" ", ""))
+            self.DetZ.setStyleSheet('color: black')
+
+        if self.main_win.is_exp_exists():
+            self.save_conf()
+
+
+    def set_dark_file(self):
+        """
+        It display a select dialog for user to select a darkfield file.
+        Parameters
+        ----------
+        none
+        Returns
+        -------
+        nothing
+        """
+        darkfield_filename = select_file(os.getcwd().replace(os.sep, '/'))
+        if darkfield_filename is not None:
+            darkfield_filename = darkfield_filename.replace(os.sep, '/')
+            self.dark_file_button.setStyleSheet("Text-align:left")
+            self.dark_file_button.setText(darkfield_filename)
+        else:
+            self.dark_file_button.setText('')
+
+
+    def set_white_file(self):
+        """
+        It display a select dialog for user to select a whitefield file.
+        Parameters
+        ----------
+        none
+        Returns
+        -------
+        nothing
+        """
+        whitefield_filename = select_file(os.getcwd().replace(os.sep, '/'))
+        if whitefield_filename is not None:
+            whitefield_filename = whitefield_filename.replace(os.sep, '/')
+            self.white_file_button.setStyleSheet("Text-align:left")
+            self.white_file_button.setText(whitefield_filename)
+        else:
+            self.white_file_button.setText('')
+
+
+    def set_data_dir(self):
+        """
+        It display a select dialog for user to select a directory with raw data file.
+        Parameters
+        ----------
+        none
+        Returns
+        -------
+        nothing
+        """
+        data_dir = select_dir(os.getcwd().replace(os.sep, '/')).replace(os.sep, '/')
+        if data_dir is not None:
+            self.data_dir_button.setStyleSheet("Text-align:left")
+            self.data_dir_button.setText(data_dir)
+        else:
+            self.data_dir_button.setText('')
+
+
+    def clear_conf(self):
+        self.diffractometer.setText('')
+        self.data_dir_button.setText('')
+        self.dark_file_button.setText('')
+        self.white_file_button.setText('')
+        self.Imult.setText('')
+        self.detector.setText('')
+        self.roi.setText('')
+        self.VFF_R.setText('')
+        self.VFF_ETA.setText('')
+        self.energy.setText('')
+        self.DetZ.setText('')
+
+
+    def load_instr_conf(self):
+        """
+        It display a select dialog for user to select a configuration file. When selected, the parameters
+        from that file will be loaded to the window.
+        Parameters
+        ----------
+        none
+        Returns
+        -------
+        nothing
+        """
+        instr_file = select_file(os.getcwd())
+        if instr_file is not None:
+            conf_map = ut.read_config(instr_file.replace(os.sep, '/'))
+            self.load_tab(conf_map)
+        else:
+            msg_window('please select valid instrument config file')
+
+
+    def get_instr_config(self):
+        """
+        It reads parameters related to instrument from the window into a dictionary.
+        Parameters
+        ----------
+        none
+        Returns
+        -------
+        conf_map : dict
+            contains parameters read from window
+        """
+        conf_map = {}
+        if len(self.diffractometer.text()) > 0:
+            conf_map['diffractometer'] = str(self.diffractometer.text())
+        if len(self.data_dir_button.text().strip()) > 0:
+            conf_map['data_dir'] = str(self.data_dir_button.text()).strip()
+        if len(self.dark_file_button.text().strip()) > 0:
+            conf_map['darkfield_filename'] = str(self.dark_file_button.text().strip())
+        if len(self.white_file_button.text().strip()) > 0:
+            conf_map['whitefield_filename'] = str(self.white_file_button.text().strip())
+        if len(self.Imult.text()) > 0:
+            conf_map['Imult'] = ast.literal_eval(str(self.Imult.text()).replace(os.linesep,''))
+        if len(self.detector.text()) > 0:
+            conf_map['detector'] = str(self.detector.text())
+        if len(self.roi.text()) > 0:
+            conf_map['roi'] = ast.literal_eval(str(self.roi.text()).replace(os.linesep,''))
+        if len(self.VFF_R.text()) > 0:
+            conf_map['VFF_R'] = ast.literal_eval(str(self.VFF_R.text()))
+        if len(self.VFF_ETA.text()) > 0:
+            conf_map['VFF_ETA'] = ast.literal_eval(str(self.VFF_ETA.text()))
+        if len(self.energy.text()) > 0:
+            conf_map['energy'] = ast.literal_eval(str(self.energy.text()))
+        if len(self.DetZ.text()) > 0:
+            conf_map['DetZ'] = ast.literal_eval(str(self.DetZ.text()))
+
+        return conf_map
+
+
+    def save_conf(self):
+        """
+        Reads the parameters needed by format display script. Saves the config_instr configuration file with parameters from the window and runs the display script.
+        Parameters
+        ----------
+        none
+        Returns
+        -------
+        nothing
+        """
+        if not self.main_win.is_exp_exists():
+            msg_window('the experiment does not exist, cannot save the config_instr file')
+            return
+
+        conf_map = self.get_instr_config()
+        if len(conf_map) == 0:
+            return
+
+        # # verify that disp configuration is ok
+        # er_msg = ver.verify('config_instr', conf_map)
+        # print('er, conf', er_msg, conf_map)
+        # if len(er_msg) > 0:
+        #     msg_window(er_msg)
+        #     if not self.main_win.no_verify:
+        #         return
+
+        ut.write_config(conf_map, ut.join(self.main_win.experiment_dir, 'conf', 'config_instr'))
---
+++ src/cohere_beamlines/aps_20ide/detectors.py
@@ -0,0 +1,274 @@
+# #########################################################################
+# Copyright (c) , UChicago Argonne, LLC. All rights reserved.             #
+#                                                                         #
+# See LICENSE file.                                                       #
+# #########################################################################
+
+import os
+import numpy as np
+import cohere_core.utilities as ut
+from abc import ABC, abstractmethod
+import h5py
+
+
+class Detector(ABC):
+    """
+    Class representing detector.
+
+    Some functions are common for all detectors and are implemented in the base class.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def files4scans(self, scans):
+        """
+        Finds directories with data that correspond to given scans or scan ranges.
+
+        :param scans : list
+            list of sub-lists defining scan ranges, ordered. For single scan a range has the same scan as beginning and end.
+            one scan example:
+            scans : [[2834, 2834]]
+            returns : [[(2834, f'{path}/data_S2834)]]
+
+            separate ranges example:
+            scans: [[2825, 2831], [2834, 2834], [2840, 2846]]
+            returns: [[(2825, f'{path}/data_S2825'), (2828, f'{path}/data_S2828'), (2831, f'{path}/data_S2831')],
+             [(2834, f'{path}/data_S2834)],
+             [(2840, f'{path}/data_S2840'), (2843, f'{path}/data_S2843'), (2846, f'{path}/data_S2846')]]
+
+        :return:
+        list of sub-lists, each sublist containing tuples with the input scans and corresponding data directories
+         within scan ranges.
+        """
+        # create empty results list that allocates a sub-list for each scan range
+        first_scan = scans[0][0]
+        last_scan = scans[-1][-1]
+        scans_files = {}
+        for scanfile in sorted(os.listdir(self.data_dir)):
+            scanfile_full = ut.join(self.data_dir, scanfile)
+            if not os.path.isfile(scanfile_full) or not scanfile_full.endswith('.h5'):
+                continue
+            # chop off the ".h5" and get the scan number
+            try:
+                scan = int(scanfile[:-3].split('_')[-1])
+            except:
+                continue
+            if scan < first_scan:
+                continue
+            elif scan > last_scan:
+                break
+            scans_files[scan] = scanfile_full
+            if scan == last_scan:
+                break
+
+        # remove excluded scans
+        scans_files = {key: value for key, value in scans_files.items() if key not in self.exclude_scans}
+
+        # distribute by ranges
+        scans_dirs_ranges = [[(k, v) for k, v in scans_files.items() if k >= scans[i][0] and k <= scans[i][-1]] for i in
+                             range(len(scans))]
+
+        # todo when time allows
+        # # remove scans that have less frames than configured. Requires to open the files.
+        # if self.min_frames > 0:
+        #     for r in scans_dirs_ranges:
+        #         for scan in r:
+        #             # open file, check number of frames
+
+        # remove empty sub-lists
+        scans_dirs_ranges = [e for e in scans_dirs_ranges if len(e) > 0]
+        return scans_dirs_ranges
+
+    def get_scan_array(self, scan_info):
+        """
+        Reads/loads raw data file and applies correction. The correction is detector dependent.
+
+        Reads raw data from a directory. The directory name is scan_info. The raw data is in form of 2D
+        frames. The frames are read, corrected and stocked into 3D data
+        This implementation is based on aps_34idc beamline.
+
+        :param scan_info: info allowing detector to retrieve data for a scan
+        :return: corrected data array
+        """
+        h5file = scan_info
+        with h5py.File(h5file, "r") as h5f:
+            arr = h5f['exchange/data'][:].T
+            if self.whitefield is None:
+                # the whitefield was not configured, try to read it from h5 file
+                try:
+                    whitefield = h5f['exchange/data_white'][:].T
+                    if np.sum(whitefield) > 0:
+                        self.whitefield = whitefield[self.roi[0]:self.roi[1], self.roi[2]:self.roi[3]]
+                        # the code below is specific to ASI detector
+                        self.wfavg = np.average(self.whitefield)
+                        self.wfstd = np.std(self.whitefield)
+                        self.whitefield = np.where(self.whitefield < self.wfavg - 3 * self.wfstd, 0, self.whitefield)
+                        if self.Imult is None:
+                            self.Imult = self.wfavg
+                except:
+                    pass
+            if self.darkfield is None:
+                # the darkfield was not configured, try to read it from h5 file
+                try:
+                    darkfield = h5f['exchange/data_dark'][:].T
+                    if np.sum(whitefield) > 0:
+                        self.darkfield = darkfield[self.roi[0]:self.roi[1], self.roi[2]:self.roi[3]]
+                        self.darkfield = np.where(self.darkfield > 0, 0.0, 1.0)
+                        if self.whitefield is not None:
+                                self.whitefield = self.darkfield * self.whitefield  # kill known bad pixel
+                except:
+                    pass
+
+        # apply roi
+        arr = arr[self.roi[0]:self.roi[1], self.roi[2]:self.roi[3], :]
+        arr = self.correct(arr)
+
+        if self.max_crop is not None:
+            def isOnedge(maxindx):
+                onedge = False
+                for i in range(len(maxindx)):
+                    if maxindx[i] == 0 or maxindx[i] > arr.shape[i] - 1:
+                        onedge = True
+                        break
+                return onedge
+
+            # check if the max value is bad pixel. If so zero it and get the next max value.
+            maxindx = np.unravel_index(arr.argmax(), arr.shape)
+            while ( isOnedge(maxindx) or
+                    arr[maxindx[0] + 1, maxindx[1], maxindx[2]] == 0
+                   and arr[maxindx[0] - 1, maxindx[1], maxindx[2]] == 0
+                   or arr[maxindx[0], maxindx[1] + 1, maxindx[2]] == 0
+                   and arr[maxindx[0], maxindx[1] - 1, maxindx[2]] == 0):
+                arr[maxindx] = 0.0
+                maxindx = np.unravel_index(arr.argmax(), arr.shape)
+
+            mc0 = self.max_crop[0] // 2
+            mc1 = self.max_crop[1] // 2
+            cropslice0 = slice(max(0, maxindx[0] - mc0), min(maxindx[0] + mc0, arr.shape[0]))
+            cropslice1 = slice(max(0, maxindx[1] - mc1), min(maxindx[1] + mc1, arr.shape[1]))
+            arr = arr[cropslice0, cropslice1, :]
+        return arr
+
+    @abstractmethod
+    def correct(self, frame):
+        """
+        Applies the correction for detector.
+
+        :param frame: 2D raw data file representing a frame
+        :return: corrected frame
+        """
+
+class ASI(Detector):
+    """
+    Subclass of Detector. Encapsulates any detector. Values are based on "34idcTIM2" detector.
+    """
+    name = "ASI"
+    dims = (518, 518)
+    roi = (0, 512, 0, 512)
+    pixel = (55.0e-6, 55e-6)
+    pixelorientation = ('x+', 'y-')  # in xrayutilities notation
+    whitefield = None
+    darkfield = None
+    max_crop = None
+    min_frames = None  # defines minimum frame scans in scan directory
+    Imult = None
+
+    def __init__(self, params):
+        super(ASI, self).__init__(self.name)
+        # The detector attributes specific for the detector.
+        # Can include data directory, whitefield_filename, roi, etc.
+        # keep parameters that are relevant to the detector
+        self.data_dir = params.get('data_dir')
+        self.roi = params.get('roi', ASI.roi)
+        self.Imult = params.get('Imult', ASI.Imult)
+        # init darkfield and whitefield if given
+        if 'whitefield_filename' in params:
+            self.whitefield = ut.read_tif(params.get('whitefield_filename'))[self.roi[0]:self.roi[1], self.roi[2]:self.roi[3]]
+            # the code below is specific to ASI detector
+            self.wfavg = np.average(self.whitefield)
+            self.wfstd = np.std(self.whitefield)
+            self.whitefield = np.where(self.whitefield < self.wfavg - 3 * self.wfstd, 0, self.whitefield)
+            self.Imult = params.get('Imult', self.wfavg)
+
+        if 'darkfield_filename' in params:
+            self.darkfield = ut.read_tif(params.get('darkfield_filename'))[self.roi[0]:self.roi[1], self.roi[2]:self.roi[3]]
+            self.darkfield = np.where(self.darkfield > 0, 0.0, 1.0)
+            if self.whitefield is not None:
+                    self.whitefield = self.darkfield * self.whitefield  # kill known bad pixel
+        self.min_frames = params.get('min_frames', None)
+        self.exclude_scans = params.get('exclude_scans', [])
+        self.max_crop = params.get('max_crop', None)
+
+
+    def correct(self, data):
+        """
+        Applies correction for the detector.
+
+        For ASI detector apply whitefield.
+
+        :param frame: 2D raw data file representing a frame
+        :return: corrected frame
+        """
+        if self.darkfield is not None:
+            if len(self.darkfield.shape) == 2:
+                cor = self.darkfield[:,:,np.newaxis]
+            else:
+                cor = self.darkfield
+            data = data * cor
+
+        if self.whitefield is not None:
+            if len(self.whitefield.shape) == 2:
+                cor = self.whitefield[:,:,np.newaxis]
+            else:
+                cor = self.whitefield
+            data = data / cor * self.Imult
+        else:
+            pass
+
+        data = np.nan_to_num(data)
+
+        return data
+
+    @staticmethod
+    def check_mandatory_params(params):
+        """
+        For the ASI detector the data directory is mandatory parameter.
+
+        :params: parameters needed to create detector
+        :return: message indicating problem or empty message if all is ok
+        """
+        if 'data_dir' not in params:
+            msg = 'data_dir parameter not configured, mandatory for 34idcTIM2 detector.'
+            raise ValueError(msg)
+        data_dir = params['data_dir']
+        if not os.path.isdir(data_dir):
+            msg = f'data_dir directory {data_dir} does not exist.'
+            raise ValueError(msg)
+
+
+def create_detector(det_name, params):
+    for detector in Detector.__subclasses__():
+        if detector.name == det_name:
+            return detector(params)
+    msg = f'detector {det_name} not defined'
+    raise ValueError(msg)
+
+
+dets = {'ASI': ASI}
+
+
+def get_pixel(det_name):
+    return dets[det_name].pixel
+
+
+def get_pixel_orientation(det_name):
+    return dets[det_name].pixelorientation
+
+
+def check_mandatory_params(det_name, params):
+    for detector in Detector.__subclasses__():
+        if detector.name == det_name:
+            return dets[det_name].check_mandatory_params(params)
+    msg = f'detector {det_name} not defined'
+    raise ValueError(msg)
